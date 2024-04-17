36c466975a6eb15e1a29ba01114bbefdf6435565
Chang Liao <20618384+changliao1025@users.noreply.github.com>
2024-04-16 22:38:43 -0600

    remove external

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,2 @@
-"%PYTHON%"  setup.py build_external -vv
 "%PYTHON%" setup.py sdist install
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-$PYTHON setup.py build_external -vv
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
